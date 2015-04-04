348228e5b0343524aeff754344d498e5ab09522f
ZhihaoZhang <zz2323@columbia.edu>
2015-04-04 13:40:47 -0400

    adding scripts

---
+++ scripts.js
@@ -0,0 +1,11 @@
+for (var i=0; i<guardianHeroinData.length; i++) {
+	var rowData = guardianHeroinData[i];
+	
+	var template = '<div class="quoteContainer">'+
+					'<div class="mainQuote">After all these years, I still have some kind of sick fascination with heroines'+
+					'<div class="wholeQuote">Heroin is a very potent drug. The high feels like a long lasting orgasm. Recovery from heroin addiction is almost a miracle. Because it is not only your body that screams for the substance. Your brain wants it too. Without heroin, emotional pain feels unbearable.</div>'+
+					'<div class="source">Katie</div>'+
+					'<div class="yearsClean">17 years clean</div>'+
+					'<div class="location">London, Uk</div>'+
+					'</div>';
+}
